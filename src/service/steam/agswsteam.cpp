6dfb52cd8430256c962fe2b77ce16747e86b801a
e <ericoporto2008@gmail.com>
2024-06-08 15:00:21 -0300

    initial sketch of agsworks steam driver

--- src/service/steam/agswsteam.cpp
+++ src/service/steam/agswsteam.cpp
@@ -1,14 +1,64 @@
 
 #include "agswsteam.h"
+#include "misc/awlog.h"
 
 namespace AGSWorks
 {
 
 void SteamWorksDriver::Init()
 {
-  // tbd
+    _s = DynApiSteam();
+    _s.Init();
+
+    SteamErrMsg errMsg;
+    ESteamAPIInitResult ini_res = _s.SteamAPI_InitFlat(&errMsg);
+    if (ini_res != ESteamAPIInitResult::k_ESteamAPIInitResult_OK)
+    {
+        std::string emsg = "SteamWorksDriver::Init error: ";
+        emsg += errMsg;
+        AWLog::LogError(emsg);
+        _s.Shutdown();
+        return;
+    }
+
+    _s.SteamAPI_ManualDispatch_Init();
+
+    _steamClient = _s.SteamInternal_CreateInterface(STEAMCLIENT_INTERFACE_VERSION);
+    HSteamUser steamUser = _s.SteamAPI_GetHSteamUser();
+    HSteamPipe steamPipe = _s.SteamAPI_GetHSteamPipe();
+    if (!_steamClient || !steamUser || !steamPipe)
+    {
+        _s.SteamAPI_Shutdown();
+        AWLog::LogError("SteamWorksDriver::Init error: ISteamClient Interface was not created!");
+        _s.Shutdown();
+        return;
+    }
+
+    _steamUserStats = _s.SteamAPI_ISteamClient_GetISteamUserStats(
+            _steamClient,
+            steamUser,
+            steamPipe,
+            STEAMUSERSTATS_INTERFACE_VERSION
+    );
+    if (!_steamUserStats)
+    {
+        _s.SteamAPI_Shutdown();
+        AWLog::LogError("SteamWorksDriver::Init error: ISteamUserStats Interface was not created!");
+        _s.Shutdown();
+        return;
+    }
+    _s.SteamAPI_ISteamUserStats_RequestCurrentStats(_steamUserStats);
+
+    _initialized = true;
 }
 
+void SteamWorksDriver::Shutdown()
+{
+    if(_initialized) {
+        _s.SteamAPI_Shutdown();
+        _s.Shutdown();
+    }
+}
 
 SteamWorksDriver* SteamWorksDriver::CreateDriver()
 {
--- src/service/steam/agswsteam.h
+++ src/service/steam/agswsteam.h
@@ -2,14 +2,23 @@
 #define __AGSWORKS_SERVICE__STEAM_H
 
 #include "service/base/agsworksdriver.h"
+#include "dynapi_steam.h"
 
 namespace AGSWorks
 {
 
 struct SteamWorksDriver : AGSWorksDriver {
+private:
+    DynApiSteam _s;
+    bool _initialized;
+    ISteamClient* _steamClient;
+    ISteamUserStats* _steamUserStats;
+
+public:
     static SteamWorksDriver* CreateDriver();
     ServiceType GetServiceType() override;
     void Init() override;
+    void Shutdown() override;
 };
 
 } // namespace AGSWorks
