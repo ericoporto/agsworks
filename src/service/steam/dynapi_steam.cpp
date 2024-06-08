61a07d9b01132ddbdf3de08c96ab6b8371557ffa
e <ericoporto2008@gmail.com>
2024-06-08 14:59:59 -0300

    DynAPI for Steam_API refactor

--- src/service/steam/dynapi_steam.cpp
+++ src/service/steam/dynapi_steam.cpp
@@ -10,195 +10,200 @@
 namespace AGSWorks
 {
 
-struct ISteamClient;
-struct ISteamUserStats;
-struct ISteamScreenshots;
-struct CallbackMsg_t {
-    int32_t m_hSteamUser;
-    int32_t m_iCallback;
-    uint8_t *m_pubParam;
-    int32_t m_cubParam;
-};
-struct SteamAPICallCompleted_t {
-    uint64_t m_hAsyncCall;
-    int32_t m_iCallback;
-    uint32_t m_cubParam;
-};
-
 static void *libHandle = nullptr;
 static struct ISteamUserStats *steamUserStats = nullptr;
 static struct ISteamScreenshots *steamScreenshots = nullptr;
 
-using C_STEAMAPI_INIT = uint8_t (*)();
-static C_STEAMAPI_INIT SteamAPI_Init = nullptr;
-using C_STEAMAPI_SHUTDOWN = void (*)();
-static C_STEAMAPI_SHUTDOWN SteamAPI_Shutdown = nullptr;
-using C_STEAMAPI_RUNCALLBACKS = void (*)();
-static C_STEAMAPI_RUNCALLBACKS SteamAPI_RunCallbacks = nullptr;
-using C_STEAMAPI_CREATEINTERFACE = struct ISteamClient *(*)(const char *);
-static C_STEAMAPI_CREATEINTERFACE SteamInternal_CreateInterface = nullptr;
-using C_STEAMAPI_GETHSTEAMUSER = int32_t (*)();
-static C_STEAMAPI_GETHSTEAMUSER SteamAPI_GetHSteamUser = nullptr;
-using C_STEAMAPI_GETHSTEAMPIPE = int32_t (*)();
-static C_STEAMAPI_GETHSTEAMPIPE SteamAPI_GetHSteamPipe = nullptr;
-using C_STEAMAPI_ISTEAMCLIENT_GETISTEAMUSERSTATS = struct ISteamUserStats * (*)(struct ISteamClient *, int32_t, int32_t, const char *);
-static C_STEAMAPI_ISTEAMCLIENT_GETISTEAMUSERSTATS SteamAPI_ISteamClient_GetISteamUserStats = nullptr;
-using C_STEAMAPI_ISTEAMUSERSTATS_REQUESTCURRENTSTATS = uint8_t (*)(struct ISteamUserStats *);
-static C_STEAMAPI_ISTEAMUSERSTATS_REQUESTCURRENTSTATS SteamAPI_ISteamUserStats_RequestCurrentStats = nullptr;
-using C_STEAMAPI_ISTEAMUSERSTATS_STORESTATS = bool (*)(struct ISteamUserStats *);
-static C_STEAMAPI_ISTEAMUSERSTATS_STORESTATS SteamAPI_ISteamUserStats_StoreStats = nullptr;
-using C_STEAMAPI_ISTEAMUSERSTATS_SETACHIEVEMENT = bool (*)(struct ISteamUserStats *, const char *);
-static C_STEAMAPI_ISTEAMUSERSTATS_SETACHIEVEMENT SteamAPI_ISteamUserStats_SetAchievement = nullptr;
-using C_STEAMAPI_ISTEAMUSERSTATS_GETACHIEVEMENT = bool (*)(struct ISteamUserStats *, const char *, bool *);
-static C_STEAMAPI_ISTEAMUSERSTATS_GETACHIEVEMENT SteamAPI_ISteamUserStats_GetAchievement = nullptr;
-using C_STEAMAPI_ISTEAMUSERSTATS_CLEARACHIEVEMENT = bool (*)(struct ISteamUserStats *, const char *, bool *);
-static C_STEAMAPI_ISTEAMUSERSTATS_CLEARACHIEVEMENT SteamAPI_ISteamUserStats_ClearAchievement = nullptr;
-using C_STEAMAPI_ISTEAMUSERSTATS_GETNUMACHIEVEMENTS = uint32_t (*)(struct ISteamUserStats *);
-static C_STEAMAPI_ISTEAMUSERSTATS_GETNUMACHIEVEMENTS SteamAPI_ISteamUserStats_GetNumAchievements = nullptr;
-using C_STEAMAPI_ISTEAMCLIENT_GETISTEAMSCREENSHOTS = struct ISteamScreenshots * (*)(struct ISteamClient *, int32_t, int32_t, const char *);
-static C_STEAMAPI_ISTEAMCLIENT_GETISTEAMSCREENSHOTS SteamAPI_ISteamClient_GetISteamScreenshots = nullptr;
-using C_STEAMAPI_ISTEAMSCREENSHOTS_HOOKSCREENSHOTS = void (*)(struct ISteamScreenshots *, uint8_t);
-static C_STEAMAPI_ISTEAMSCREENSHOTS_HOOKSCREENSHOTS SteamAPI_ISteamScreenshots_HookScreenshots = nullptr;
-using C_STEAMAPI_ISTEAMSCREENSHOTS_WRITESCREENSHOT = uint32_t (*)(struct ISteamScreenshots *, void *, uint32_t, int32_t, int32_t);
-static C_STEAMAPI_ISTEAMSCREENSHOTS_WRITESCREENSHOT SteamAPI_ISteamScreenshots_WriteScreenshot = nullptr;
-using C_STEAMAPI_MANUALDISPATCH_INIT = void (*)();
-static C_STEAMAPI_MANUALDISPATCH_INIT SteamAPI_ManualDispatch_Init = nullptr;
-using C_STEAMAPI_MANUALDISPATCH_RUNFRAME = void (*)(int32_t);
-static C_STEAMAPI_MANUALDISPATCH_RUNFRAME SteamAPI_ManualDispatch_RunFrame = nullptr;
-using C_STEAMAPI_MANUALDISPATCH_GETNEXTCALLBACK = uint8_t (*)(int32_t, struct CallbackMsg_t *);
-static C_STEAMAPI_MANUALDISPATCH_GETNEXTCALLBACK SteamAPI_ManualDispatch_GetNextCallback = nullptr;
-using C_STEAMAPI_MANUALDISPATCH_FREELASTCALLBACK = void (*)(int32_t);
-static C_STEAMAPI_MANUALDISPATCH_FREELASTCALLBACK SteamAPI_ManualDispatch_FreeLastCallback = nullptr;
-using C_STEAMAPI_MANUALDISPATCH_GETAPICALLRESULT =  uint8_t (*)(int32_t, uint64_t, void *, int32_t, int32_t, uint8_t *);
-static C_STEAMAPI_MANUALDISPATCH_GETAPICALLRESULT SteamAPI_ManualDispatch_GetAPICallResult = nullptr;
 
-static void CleanUp()
+void DynApiSteam::ClearFuncsToNull()
 {
     libHandle = nullptr;
     steamUserStats = nullptr;
+    c_SteamAPI_InitFlat = nullptr;
+    c_SteamAPI_Shutdown = nullptr;
+    c_SteamAPI_RunCallbacks = nullptr;
+    c_SteamInternal_CreateInterface = nullptr;
+    c_SteamAPI_GetHSteamUser = nullptr;
+    c_SteamAPI_GetHSteamPipe = nullptr;
+    c_SteamAPI_ISteamClient_GetISteamUserStats = nullptr;
+    c_SteamAPI_ISteamUserStats_RequestCurrentStats = nullptr;
+    c_SteamAPI_ISteamUserStats_StoreStats = nullptr;
+    c_SteamAPI_ISteamUserStats_SetAchievement = nullptr;
+    c_SteamAPI_ISteamUserStats_GetAchievement = nullptr;
+    c_SteamAPI_ISteamUserStats_ClearAchievement = nullptr;
+    c_SteamAPI_ISteamUserStats_GetNumAchievements = nullptr;
+    c_SteamAPI_ISteamClient_GetISteamScreenshots = nullptr;
+    c_SteamAPI_ISteamScreenshots_HookScreenshots = nullptr;
+    c_SteamAPI_ISteamScreenshots_WriteScreenshot = nullptr;
+    c_SteamAPI_ManualDispatch_Init = nullptr;
+    c_SteamAPI_ManualDispatch_RunFrame = nullptr;
+    c_SteamAPI_ManualDispatch_GetNextCallback = nullptr;
+    c_SteamAPI_ManualDispatch_FreeLastCallback = nullptr;
+    c_SteamAPI_ManualDispatch_GetAPICallResult = nullptr;
+}
 
-    SteamAPI_Init = nullptr;
-    SteamAPI_Shutdown = nullptr;
-    SteamAPI_RunCallbacks = nullptr;
-    SteamInternal_CreateInterface = nullptr;
-    SteamAPI_GetHSteamUser = nullptr;
-    SteamAPI_GetHSteamPipe = nullptr;
-    SteamAPI_ISteamClient_GetISteamUserStats = nullptr;
-    SteamAPI_ISteamUserStats_RequestCurrentStats = nullptr;
-    SteamAPI_ISteamUserStats_StoreStats = nullptr;
-    SteamAPI_ISteamUserStats_SetAchievement = nullptr;
-    SteamAPI_ISteamUserStats_GetAchievement = nullptr;
-    SteamAPI_ISteamUserStats_ClearAchievement = nullptr;
-    SteamAPI_ISteamUserStats_GetNumAchievements = nullptr;
-    SteamAPI_ISteamClient_GetISteamScreenshots = nullptr;
-    SteamAPI_ISteamScreenshots_HookScreenshots = nullptr;
-    SteamAPI_ISteamScreenshots_WriteScreenshot = nullptr;
-    SteamAPI_ManualDispatch_Init = nullptr;
-    SteamAPI_ManualDispatch_RunFrame = nullptr;
-    SteamAPI_ManualDispatch_GetNextCallback = nullptr;
-    SteamAPI_ManualDispatch_FreeLastCallback = nullptr;
-    SteamAPI_ManualDispatch_GetAPICallResult = nullptr;
+void DynApiSteam::Shutdown()
+{
+    ClearFuncsToNull();
 }
 
 
-static void *LoadFunc(const char* fn) {
+void * DynApiSteam::LoadFunc(const char* fn) {
     std::string sfn = fn;
     void* pfn = DynamicServiceLoader::GetFunctionAddress(sfn);
     if(!pfn) {
         std::string msg = "steam_api: symbol '" + sfn + "' not found!";
         AWLog::LogError(msg);
-        CleanUp();
+        Shutdown();
         return nullptr;
     }
     return pfn;
 }
 
-static bool Init() {
-    SteamAPI_Init = (C_STEAMAPI_INIT) (intptr_t) LoadFunc("SteamAPI_Init");
-    if (!SteamAPI_Init) {
+bool DynApiSteam::Init() {
+    ClearFuncsToNull();
+    c_SteamAPI_InitFlat = (C_STEAMAPI_INITFLAT ) (intptr_t) LoadFunc("c_SteamAPI_InitFlat");
+    if (!c_SteamAPI_InitFlat) {
         return false;
     }
-    SteamAPI_Shutdown = (C_STEAMAPI_SHUTDOWN) (intptr_t) LoadFunc("SteamAPI_Shutdown");
-    if (!SteamAPI_Shutdown) {
+    c_SteamAPI_Shutdown = (C_STEAMAPI_SHUTDOWN) (intptr_t) LoadFunc("c_SteamAPI_Shutdown");
+    if (!c_SteamAPI_Shutdown) {
         return false;
     }
-    SteamAPI_RunCallbacks = (C_STEAMAPI_RUNCALLBACKS) (intptr_t) LoadFunc("SteamAPI_RunCallbacks");
-    if (!SteamAPI_RunCallbacks) {
+    c_SteamAPI_RunCallbacks = (C_STEAMAPI_RUNCALLBACKS) (intptr_t) LoadFunc("c_SteamAPI_RunCallbacks");
+    if (!c_SteamAPI_RunCallbacks) {
         return false;
     }
-    SteamInternal_CreateInterface = (C_STEAMAPI_CREATEINTERFACE) (intptr_t) LoadFunc("SteamInternal_CreateInterface");
-    if (!SteamInternal_CreateInterface) {
+    c_SteamInternal_CreateInterface = (C_STEAMINTERNAL_CREATEINTERFACE) (intptr_t) LoadFunc("c_SteamInternal_CreateInterface");
+    if (!c_SteamInternal_CreateInterface) {
         return false;
     }
-    SteamAPI_GetHSteamUser = (C_STEAMAPI_GETHSTEAMUSER) (intptr_t) LoadFunc("SteamAPI_GetHSteamUser");
-    if (!SteamAPI_GetHSteamUser) {
+    c_SteamAPI_GetHSteamUser = (C_STEAMAPI_GETHSTEAMUSER) (intptr_t) LoadFunc("c_SteamAPI_GetHSteamUser");
+    if (!c_SteamAPI_GetHSteamUser) {
         return false;
     }
-    SteamAPI_GetHSteamPipe = (C_STEAMAPI_GETHSTEAMPIPE) (intptr_t) LoadFunc("SteamAPI_GetHSteamPipe");
-    if (!SteamAPI_GetHSteamPipe) {
+    c_SteamAPI_GetHSteamPipe = (C_STEAMAPI_GETHSTEAMPIPE) (intptr_t) LoadFunc("c_SteamAPI_GetHSteamPipe");
+    if (!c_SteamAPI_GetHSteamPipe) {
         return false;
     }
-    SteamAPI_ISteamClient_GetISteamUserStats = (C_STEAMAPI_ISTEAMCLIENT_GETISTEAMUSERSTATS) (intptr_t) LoadFunc("SteamAPI_ISteamClient_GetISteamUserStats");
-    if (!SteamAPI_ISteamClient_GetISteamUserStats) {
+    c_SteamAPI_ISteamClient_GetISteamUserStats = (C_STEAMAPI_ISTEAMCLIENT_GETISTEAMUSERSTATS) (intptr_t) LoadFunc("c_SteamAPI_ISteamClient_GetISteamUserStats");
+    if (!c_SteamAPI_ISteamClient_GetISteamUserStats) {
         return false;
     }
-    SteamAPI_ISteamUserStats_RequestCurrentStats = (C_STEAMAPI_ISTEAMUSERSTATS_REQUESTCURRENTSTATS) (intptr_t) LoadFunc("SteamAPI_ISteamUserStats_RequestCurrentStats");
-    if (!SteamAPI_ISteamUserStats_RequestCurrentStats) {
+    c_SteamAPI_ISteamUserStats_RequestCurrentStats = (C_STEAMAPI_ISTEAMUSERSTATS_REQUESTCURRENTSTATS) (intptr_t) LoadFunc("c_SteamAPI_ISteamUserStats_RequestCurrentStats");
+    if (!c_SteamAPI_ISteamUserStats_RequestCurrentStats) {
         return false;
     }
-    SteamAPI_ISteamUserStats_StoreStats = (C_STEAMAPI_ISTEAMUSERSTATS_STORESTATS) (intptr_t) LoadFunc("SteamAPI_ISteamUserStats_StoreStats");
-    if (!SteamAPI_ISteamUserStats_StoreStats) {
+    c_SteamAPI_ISteamUserStats_StoreStats = (C_STEAMAPI_ISTEAMUSERSTATS_STORESTATS) (intptr_t) LoadFunc("c_SteamAPI_ISteamUserStats_StoreStats");
+    if (!c_SteamAPI_ISteamUserStats_StoreStats) {
         return false;
     }
-    SteamAPI_ISteamUserStats_SetAchievement = (C_STEAMAPI_ISTEAMUSERSTATS_SETACHIEVEMENT) (intptr_t) LoadFunc("SteamAPI_ISteamUserStats_SetAchievement");
-    if (!SteamAPI_ISteamUserStats_SetAchievement) {
+    c_SteamAPI_ISteamUserStats_SetAchievement = (C_STEAMAPI_ISTEAMUSERSTATS_SETACHIEVEMENT) (intptr_t) LoadFunc("c_SteamAPI_ISteamUserStats_SetAchievement");
+    if (!c_SteamAPI_ISteamUserStats_SetAchievement) {
         return false;
     }
-    SteamAPI_ISteamUserStats_GetAchievement = (C_STEAMAPI_ISTEAMUSERSTATS_GETACHIEVEMENT) (intptr_t) LoadFunc("SteamAPI_ISteamUserStats_GetAchievement");
-    if (!SteamAPI_ISteamUserStats_GetAchievement) {
+    c_SteamAPI_ISteamUserStats_GetAchievement = (C_STEAMAPI_ISTEAMUSERSTATS_GETACHIEVEMENT) (intptr_t) LoadFunc("c_SteamAPI_ISteamUserStats_GetAchievement");
+    if (!c_SteamAPI_ISteamUserStats_GetAchievement) {
         return false;
     }
-    SteamAPI_ISteamUserStats_ClearAchievement = (C_STEAMAPI_ISTEAMUSERSTATS_CLEARACHIEVEMENT) (intptr_t) LoadFunc("SteamAPI_ISteamUserStats_ClearAchievement");
-    if (!SteamAPI_ISteamUserStats_ClearAchievement) {
+    c_SteamAPI_ISteamUserStats_ClearAchievement = (C_STEAMAPI_ISTEAMUSERSTATS_CLEARACHIEVEMENT) (intptr_t) LoadFunc("c_SteamAPI_ISteamUserStats_ClearAchievement");
+    if (!c_SteamAPI_ISteamUserStats_ClearAchievement) {
         return false;
     }
-    SteamAPI_ISteamUserStats_GetNumAchievements = (C_STEAMAPI_ISTEAMUSERSTATS_GETNUMACHIEVEMENTS) (intptr_t) LoadFunc("SteamAPI_ISteamUserStats_GetNumAchievements");
-    if (!SteamAPI_ISteamUserStats_GetNumAchievements) {
+    c_SteamAPI_ISteamUserStats_GetNumAchievements = (C_STEAMAPI_ISTEAMUSERSTATS_GETNUMACHIEVEMENTS) (intptr_t) LoadFunc("c_SteamAPI_ISteamUserStats_GetNumAchievements");
+    if (!c_SteamAPI_ISteamUserStats_GetNumAchievements) {
         return false;
     }
-    SteamAPI_ISteamClient_GetISteamScreenshots = (C_STEAMAPI_ISTEAMCLIENT_GETISTEAMSCREENSHOTS) (intptr_t) LoadFunc("SteamAPI_ISteamClient_GetISteamScreenshots");
-    if (!SteamAPI_ISteamClient_GetISteamScreenshots) {
+    c_SteamAPI_ISteamClient_GetISteamScreenshots = (C_STEAMAPI_ISTEAMCLIENT_GETISTEAMSCREENSHOTS) (intptr_t) LoadFunc("c_SteamAPI_ISteamClient_GetISteamScreenshots");
+    if (!c_SteamAPI_ISteamClient_GetISteamScreenshots) {
         return false;
     }
-    SteamAPI_ISteamScreenshots_HookScreenshots = (C_STEAMAPI_ISTEAMSCREENSHOTS_HOOKSCREENSHOTS) (intptr_t) LoadFunc("SteamAPI_ISteamScreenshots_HookScreenshots");
-    if (!SteamAPI_ISteamScreenshots_HookScreenshots) {
+    c_SteamAPI_ISteamScreenshots_HookScreenshots = (C_STEAMAPI_ISTEAMSCREENSHOTS_HOOKSCREENSHOTS) (intptr_t) LoadFunc("c_SteamAPI_ISteamScreenshots_HookScreenshots");
+    if (!c_SteamAPI_ISteamScreenshots_HookScreenshots) {
         return false;
     }
-    SteamAPI_ISteamScreenshots_WriteScreenshot = (C_STEAMAPI_ISTEAMSCREENSHOTS_WRITESCREENSHOT) (intptr_t) LoadFunc("SteamAPI_ISteamScreenshots_WriteScreenshot");
-    if (!SteamAPI_ISteamScreenshots_WriteScreenshot) {
+    c_SteamAPI_ISteamScreenshots_WriteScreenshot = (C_STEAMAPI_ISTEAMSCREENSHOTS_WRITESCREENSHOT) (intptr_t) LoadFunc("c_SteamAPI_ISteamScreenshots_WriteScreenshot");
+    if (!c_SteamAPI_ISteamScreenshots_WriteScreenshot) {
         return false;
     }
-    SteamAPI_ManualDispatch_Init = (C_STEAMAPI_MANUALDISPATCH_INIT) (intptr_t) LoadFunc("SteamAPI_ManualDispatch_Init");
-    if (!SteamAPI_ManualDispatch_Init) {
+    c_SteamAPI_ManualDispatch_Init = (C_STEAMAPI_MANUALDISPATCH_INIT) (intptr_t) LoadFunc("c_SteamAPI_ManualDispatch_Init");
+    if (!c_SteamAPI_ManualDispatch_Init) {
         return false;
     }
-    SteamAPI_ManualDispatch_RunFrame = (C_STEAMAPI_MANUALDISPATCH_RUNFRAME) (intptr_t) LoadFunc("SteamAPI_ManualDispatch_RunFrame");
-    if (!SteamAPI_ManualDispatch_RunFrame) {
+    c_SteamAPI_ManualDispatch_RunFrame = (C_STEAMAPI_MANUALDISPATCH_RUNFRAME) (intptr_t) LoadFunc("c_SteamAPI_ManualDispatch_RunFrame");
+    if (!c_SteamAPI_ManualDispatch_RunFrame) {
         return false;
     }
-    SteamAPI_ManualDispatch_GetNextCallback = (C_STEAMAPI_MANUALDISPATCH_GETNEXTCALLBACK) (intptr_t) LoadFunc("SteamAPI_ManualDispatch_GetNextCallback");
-    if (!SteamAPI_ManualDispatch_GetNextCallback) {
+    c_SteamAPI_ManualDispatch_GetNextCallback = (C_STEAMAPI_MANUALDISPATCH_GETNEXTCALLBACK) (intptr_t) LoadFunc("c_SteamAPI_ManualDispatch_GetNextCallback");
+    if (!c_SteamAPI_ManualDispatch_GetNextCallback) {
         return false;
     }
-    SteamAPI_ManualDispatch_FreeLastCallback = (C_STEAMAPI_MANUALDISPATCH_FREELASTCALLBACK) (intptr_t) LoadFunc("SteamAPI_ManualDispatch_FreeLastCallback");
-    if (!SteamAPI_ManualDispatch_FreeLastCallback) {
+    c_SteamAPI_ManualDispatch_FreeLastCallback = (C_STEAMAPI_MANUALDISPATCH_FREELASTCALLBACK) (intptr_t) LoadFunc("c_SteamAPI_ManualDispatch_FreeLastCallback");
+    if (!c_SteamAPI_ManualDispatch_FreeLastCallback) {
         return false;
     }
-    SteamAPI_ManualDispatch_GetAPICallResult = (C_STEAMAPI_MANUALDISPATCH_GETAPICALLRESULT) (intptr_t) LoadFunc("SteamAPI_ManualDispatch_GetAPICallResult");
-    if (!SteamAPI_ManualDispatch_GetAPICallResult) {
+    c_SteamAPI_ManualDispatch_GetAPICallResult = (C_STEAMAPI_MANUALDISPATCH_GETAPICALLRESULT) (intptr_t) LoadFunc("c_SteamAPI_ManualDispatch_GetAPICallResult");
+    if (!c_SteamAPI_ManualDispatch_GetAPICallResult) {
         return false;
     }
     return true;
 }
+
+ESteamAPIInitResult DynApiSteam::SteamAPI_InitFlat(SteamErrMsg *pOutErrMsg) {
+    return static_cast<ESteamAPIInitResult>(c_SteamAPI_InitFlat(pOutErrMsg));
+}
+
+void DynApiSteam::SteamAPI_Shutdown() {
+    c_SteamAPI_Shutdown();
+}
+
+void DynApiSteam::SteamAPI_RunCallbacks() {
+    c_SteamAPI_RunCallbacks();
+}
+
+bool DynApiSteam::SteamAPI_ISteamUserStats_StoreStats(ISteamUserStats *self) {
+    return c_SteamAPI_ISteamUserStats_StoreStats(self);
+}
+
+bool DynApiSteam::SteamAPI_ISteamUserStats_SetAchievement(ISteamUserStats *self, const char *pchName) {
+    return c_SteamAPI_ISteamUserStats_SetAchievement(self, pchName);
+}
+
+bool DynApiSteam::SteamAPI_ISteamUserStats_GetAchievement(ISteamUserStats *self, const char *pchName, bool *pbAchieved) {
+    return c_SteamAPI_ISteamUserStats_GetAchievement(self, pchName, pbAchieved);
+}
+
+bool DynApiSteam::SteamAPI_ISteamUserStats_ClearAchievement(ISteamUserStats *self, const char *pchName) {
+    return c_SteamAPI_ISteamUserStats_ClearAchievement(self, pchName);
+}
+
+uint32_t DynApiSteam::SteamAPI_ISteamUserStats_GetNumAchievements(ISteamUserStats *self) {
+    return c_SteamAPI_ISteamUserStats_GetNumAchievements(self);
+}
+
+bool DynApiSteam::SteamAPI_ISteamUserStats_RequestCurrentStats(ISteamUserStats *self) {
+    return c_SteamAPI_ISteamUserStats_RequestCurrentStats(self);
+}
+
+ISteamUserStats *DynApiSteam::SteamAPI_ISteamClient_GetISteamUserStats(ISteamClient *self, HSteamUser hSteamUser, HSteamPipe hSteamPipe, const char *pchVersion) {
+    return c_SteamAPI_ISteamClient_GetISteamUserStats(self, hSteamUser, hSteamPipe, pchVersion);
+}
+
+void DynApiSteam::SteamAPI_ManualDispatch_Init() {
+    c_SteamAPI_ManualDispatch_Init();
+}
+
+ISteamClient *DynApiSteam::SteamInternal_CreateInterface(const char *ver) {
+    return c_SteamInternal_CreateInterface(ver);
+}
+
+HSteamUser DynApiSteam::SteamAPI_GetHSteamUser() {
+    return c_SteamAPI_GetHSteamUser();
+}
+
+HSteamPipe DynApiSteam::SteamAPI_GetHSteamPipe() {
+    return c_SteamAPI_GetHSteamPipe();
+}
+
 
 } // namespace AGSWorks
--- src/service/steam/dynapi_steam.h
+++ src/service/steam/dynapi_steam.h
@@ -7,9 +7,110 @@
 
 #include <cstdint>
 
-class dynapi_steam {
+namespace AGSWorks {
+// handle to a communication pipe to the Steam client
+    typedef int32_t HSteamPipe;
+// handle to single instance of a steam user
+    typedef int32_t HSteamUser;
 
-};
+    struct ISteamClient;
+    struct ISteamScreenshots;
+    struct ISteamUserStats;
 
+    struct CallbackMsg_t {
+        HSteamUser m_hSteamUser;
+        int32_t m_iCallback;
+        uint8_t *m_pubParam;
+        int32_t m_cubParam;
+    };
+    struct SteamAPICallCompleted_t {
+        uint64_t m_hAsyncCall;
+        int32_t m_iCallback;
+        uint32_t m_cubParam;
+    };
+
+    const char * STEAMCLIENT_INTERFACE_VERSION = "SteamClient021";
+    const char * STEAMUSERSTATS_INTERFACE_VERSION = "STEAMUSERSTATS_INTERFACE_VERSION012";
+
+    const int k_cchMaxSteamErrMsg = 1024;
+    typedef char SteamErrMsg[ k_cchMaxSteamErrMsg ];
+
+    enum ESteamAPIInitResult
+    {
+        k_ESteamAPIInitResult_OK = 0,
+        k_ESteamAPIInitResult_FailedGeneric = 1, // Some other failure
+        k_ESteamAPIInitResult_NoSteamClient = 2, // We cannot connect to Steam, steam probably isn't running
+        k_ESteamAPIInitResult_VersionMismatch = 3, // Steam client appears to be out of date
+    };
+
+    class DynApiSteam {
+    private:
+        void* LoadFunc(const char* fn);
+        void ClearFuncsToNull();
+
+        using C_STEAMAPI_INITFLAT = int32_t (*)(SteamErrMsg *);
+        C_STEAMAPI_INITFLAT c_SteamAPI_InitFlat;
+        using C_STEAMAPI_SHUTDOWN = void (*)();
+        C_STEAMAPI_SHUTDOWN c_SteamAPI_Shutdown;
+        using C_STEAMAPI_RUNCALLBACKS = void (*)();
+        C_STEAMAPI_RUNCALLBACKS c_SteamAPI_RunCallbacks;
+        using C_STEAMINTERNAL_CREATEINTERFACE = struct ISteamClient *(*)(const char *);
+        C_STEAMINTERNAL_CREATEINTERFACE c_SteamInternal_CreateInterface;
+        using C_STEAMAPI_GETHSTEAMUSER = int32_t (*)();
+        C_STEAMAPI_GETHSTEAMUSER c_SteamAPI_GetHSteamUser;
+        using C_STEAMAPI_GETHSTEAMPIPE = int32_t (*)();
+        C_STEAMAPI_GETHSTEAMPIPE c_SteamAPI_GetHSteamPipe;
+        using C_STEAMAPI_ISTEAMCLIENT_GETISTEAMUSERSTATS = struct ISteamUserStats * (*)(struct ISteamClient *, int32_t, int32_t, const char *);
+        C_STEAMAPI_ISTEAMCLIENT_GETISTEAMUSERSTATS c_SteamAPI_ISteamClient_GetISteamUserStats;
+        using C_STEAMAPI_ISTEAMUSERSTATS_REQUESTCURRENTSTATS = uint8_t (*)(struct ISteamUserStats *);
+        C_STEAMAPI_ISTEAMUSERSTATS_REQUESTCURRENTSTATS c_SteamAPI_ISteamUserStats_RequestCurrentStats;
+        using C_STEAMAPI_ISTEAMUSERSTATS_STORESTATS = bool (*)(struct ISteamUserStats *);
+        C_STEAMAPI_ISTEAMUSERSTATS_STORESTATS c_SteamAPI_ISteamUserStats_StoreStats;
+        using C_STEAMAPI_ISTEAMUSERSTATS_SETACHIEVEMENT = bool (*)(struct ISteamUserStats *, const char *);
+        C_STEAMAPI_ISTEAMUSERSTATS_SETACHIEVEMENT c_SteamAPI_ISteamUserStats_SetAchievement;
+        using C_STEAMAPI_ISTEAMUSERSTATS_GETACHIEVEMENT = bool (*)(struct ISteamUserStats *, const char *, bool *);
+        C_STEAMAPI_ISTEAMUSERSTATS_GETACHIEVEMENT c_SteamAPI_ISteamUserStats_GetAchievement;
+        using C_STEAMAPI_ISTEAMUSERSTATS_CLEARACHIEVEMENT = bool (*)(struct ISteamUserStats *, const char *, bool *);
+        C_STEAMAPI_ISTEAMUSERSTATS_CLEARACHIEVEMENT c_SteamAPI_ISteamUserStats_ClearAchievement;
+        using C_STEAMAPI_ISTEAMUSERSTATS_GETNUMACHIEVEMENTS = uint32_t (*)(struct ISteamUserStats *);
+        C_STEAMAPI_ISTEAMUSERSTATS_GETNUMACHIEVEMENTS c_SteamAPI_ISteamUserStats_GetNumAchievements;
+        using C_STEAMAPI_ISTEAMCLIENT_GETISTEAMSCREENSHOTS = struct ISteamScreenshots * (*)(struct ISteamClient *, int32_t, int32_t, const char *);
+        C_STEAMAPI_ISTEAMCLIENT_GETISTEAMSCREENSHOTS c_SteamAPI_ISteamClient_GetISteamScreenshots;
+        using C_STEAMAPI_ISTEAMSCREENSHOTS_HOOKSCREENSHOTS = void (*)(struct ISteamScreenshots *, uint8_t);
+        C_STEAMAPI_ISTEAMSCREENSHOTS_HOOKSCREENSHOTS c_SteamAPI_ISteamScreenshots_HookScreenshots;
+        using C_STEAMAPI_ISTEAMSCREENSHOTS_WRITESCREENSHOT = uint32_t (*)(struct ISteamScreenshots *, void *, uint32_t, int32_t, int32_t);
+        C_STEAMAPI_ISTEAMSCREENSHOTS_WRITESCREENSHOT c_SteamAPI_ISteamScreenshots_WriteScreenshot;
+        using C_STEAMAPI_MANUALDISPATCH_INIT = void (*)();
+        C_STEAMAPI_MANUALDISPATCH_INIT c_SteamAPI_ManualDispatch_Init;
+        using C_STEAMAPI_MANUALDISPATCH_RUNFRAME = void (*)(int32_t);
+        C_STEAMAPI_MANUALDISPATCH_RUNFRAME c_SteamAPI_ManualDispatch_RunFrame;
+        using C_STEAMAPI_MANUALDISPATCH_GETNEXTCALLBACK = uint8_t (*)(int32_t, struct CallbackMsg_t *);
+        C_STEAMAPI_MANUALDISPATCH_GETNEXTCALLBACK c_SteamAPI_ManualDispatch_GetNextCallback;
+        using C_STEAMAPI_MANUALDISPATCH_FREELASTCALLBACK = void (*)(int32_t);
+        C_STEAMAPI_MANUALDISPATCH_FREELASTCALLBACK c_SteamAPI_ManualDispatch_FreeLastCallback;
+        using C_STEAMAPI_MANUALDISPATCH_GETAPICALLRESULT =  uint8_t (*)(int32_t, uint64_t, void *, int32_t, int32_t, uint8_t *);
+        C_STEAMAPI_MANUALDISPATCH_GETAPICALLRESULT c_SteamAPI_ManualDispatch_GetAPICallResult;
+
+    public:
+        bool Init();
+        void Shutdown();
+
+        ESteamAPIInitResult SteamAPI_InitFlat( SteamErrMsg *pOutErrMsg );
+        void SteamAPI_Shutdown();
+        void SteamAPI_RunCallbacks();
+        bool SteamAPI_ISteamUserStats_StoreStats( ISteamUserStats* self );
+        bool SteamAPI_ISteamUserStats_SetAchievement( ISteamUserStats* self, const char * pchName );
+        bool SteamAPI_ISteamUserStats_GetAchievement( ISteamUserStats* self, const char * pchName, bool * pbAchieved );
+        bool SteamAPI_ISteamUserStats_ClearAchievement( ISteamUserStats* self, const char * pchName );
+        uint32_t SteamAPI_ISteamUserStats_GetNumAchievements( ISteamUserStats* self );
+        bool SteamAPI_ISteamUserStats_RequestCurrentStats( ISteamUserStats* self );
+        ISteamUserStats * SteamAPI_ISteamClient_GetISteamUserStats( ISteamClient* self, HSteamUser hSteamUser, HSteamPipe hSteamPipe, const char * pchVersion );
+        void SteamAPI_ManualDispatch_Init();
+        ISteamClient *SteamInternal_CreateInterface(const char *string);
+        HSteamUser SteamAPI_GetHSteamUser();
+        HSteamPipe SteamAPI_GetHSteamPipe();
+    };
+
+}
 
 #endif //AGSWORKS_DYNAPI_STEAM_H
